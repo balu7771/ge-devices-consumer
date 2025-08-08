69935e8c418ad501408f6ed047f3ca6f640685b4
balu learnz <balaji.dreamz@gmail.com>
2025-08-08 07:03:58 +0530

    initial commit.

---
+++ src/main/java/com/health/ge/jw/GEDevicesConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.health.ge.jw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GEDevicesConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GEDevicesConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/health/ge/jw/ge_devices_consumer/GEDevicesConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.ge.jw.ge_devices_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GEDevicesConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
